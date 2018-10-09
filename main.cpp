e0f76584fc02e8c2461a26021dd687a88a313a32
Efarov Ilya <efarov.ily@mail.ru>
2018-10-10 01:21:26 +0900

    merge c1 && master

--- main.cpp
+++ main.cpp
@@ -12,10 +12,8 @@ int main()
     Matrix matr1(5, 5);
     matr1.randint(1, 10);
     matr1.print_matrix(3, 0);
-    matr1.delta(down);
-    matr1.print_matrix(3, 0);
-//    FuncFillMatrix lambda1 = [](double x, unsigned i, unsigned j)->double{return pow(x, i*2 + j*2);};
-//    matr1.fill_matrix(lambda1);
-//    matr1.print_matrix(13, 0);
+    FuncFillMatrix lambda1 = [](double x, unsigned i, unsigned j)->double{return pow(x, i*2 + j*2);};
+    matr1.fill_matrix(lambda1);
+    matr1.print_matrix(13, 0);
     return 0;
 }
