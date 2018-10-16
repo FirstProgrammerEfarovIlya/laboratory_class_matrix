45d0864db6576f2c586dfc6e37dbddd7f60e52ec
Efarov Ilya <efarov.ily@mail.ru>
2018-10-16 22:42:22 +0900

    end_work

--- main.cpp
+++ main.cpp
@@ -9,19 +9,19 @@ using namespace algebra;
 
 int main()
 {
-    Matrix matr1(3, 4);
+    Matrix matr1(2, 3);
     vectord x;
     input_matrix(matr1);
-    print_matrix(matr1, 3, 0);
-    FuncFillMatrix lambda1 = [](double x, unsigned i, unsigned j)->double{return pow(x, i*2 + j*2);};
-    matr1.fill_matrix(lambda1);
-//    matr1 = method_gauss(matr1);
-//    x = reverse_step(matr1);
+//    print_matrix(matr1, 3, 0);
+//    FuncFillMatrix lambda1 = [](double x, unsigned i, unsigned j)->double{return pow(x, i*2 + j*2);};
+//    matr1.fill_matrix(lambda1);
+    matr1 = method_gauss(matr1);
+    x = reverse_step(matr1);
     print_matrix(matr1, 13, 8);
-//    for (unsigned i = 0; i < x.size(); i++)
-//    {
-//        cout << "x" << i + 1 << " = " << x[i] << "; ";
-//    }
-//    cout << endl;
+    for (unsigned i = 0; i < x.size(); i++)
+    {
+        cout << "x" << i + 1 << " = " << x[i] << "; ";
+    }
+    cout << endl;
     return 0;
 }
